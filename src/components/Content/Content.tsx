57635df7c7984efdba7cc2dccffce59ed3e065dc
timothyc3 <timothyc2@Gmail.com>
2022-05-11 20:13:28 +0800

    Added MessageInput, refactoring the message input box to a separate component.

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,16 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
 import MessageContainer from "./components/MessageContainer";
+import MessageInput from "./components/MessageInput";
 
 export default function Content() {
     return (
         <div className="bg-main-content-black grid grid-rows-[3rem_1fr_5rem] w-full h-full">
             <Header />
             <MessageContainer />
-            <input type="search" placeholder="MessageItem"
-                   className="bg-chat-box-search-bar-main m-5 rounded-lg px-5 text-white
-                   text-sm placeholder-inactive-light-grey focus:outline-0"/>
-
+            <MessageInput />
         </div>
     );
 }
