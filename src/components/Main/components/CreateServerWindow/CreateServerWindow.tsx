70157538c0bb3e0d41a55a25ae015060bc384a8c
Tim <timothyc2@Gmail.com>
2022-06-23 21:57:14 +0800

    Added createServer into the activeSlice which will handle the create server window popping up without affecting levelOne and levelTwo

--- src/components/Main/components/CreateServerWindow/CreateServerWindow.tsx
+++ src/components/Main/components/CreateServerWindow/CreateServerWindow.tsx
@@ -13,7 +13,8 @@ export default function CreateServerWindow() {
                 createServerOpen && <div className="bg-black/70 w-full h-full fixed flex justify-center items-center">
                     <div className="relative bg-white w-108 h-144 rounded-md pt-6 grid grid-rows-[auto_auto_1fr_auto]">
                         <svg xmlns="http://www.w3.org/2000/svg"
-                             className="h-7 w-7 absolute right-4 top-4 text-inactive-light-grey"
+                             className="h-7 w-7 absolute right-3.5 top-3.5 text-inactive-light-grey hover:text-server-bar-black
+                             transition-colors"
                              fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                             <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                         </svg>
