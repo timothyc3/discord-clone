2e39abbee051ad576cc270f6a84c06c6e1076024
Tim <timothyc2@Gmail.com>
2022-06-27 22:49:11 +0800

    amended the hover color styling for icons on the server menu from clicking header.tsx

--- src/components/Main/components/ChannelSideBar/components/Header.tsx
+++ src/components/Main/components/ChannelSideBar/components/Header.tsx
@@ -62,7 +62,7 @@ export default function Header(props: {
                     first:mt-0 last:mb-0 group transition-all hover:bg-darker-blue">
                         <h2 className="text-blue group-hover:text-white">Invite People</h2>
                         <svg xmlns="http://www.w3.org/2000/svg"
-                             className={`h-5 w-5 text-blue group-hover:text-white`}
+                             className="h-5 w-5 text-blue group-hover:text-white"
                              viewBox="0 0 20 20" fill="currentColor">
                             <path
                                 d="M8 9a3 3 0 100-6 3 3 0 000 6zM8 11a6 6 0 016 6H2a6 6 0 016-6zM16 7a1 1 0 10-2 0v1h-1a1 1 0 100 2h1v1a1 1 0 102 0v-1h1a1 1 0 100-2h-1V7z"/>
@@ -73,7 +73,7 @@ export default function Header(props: {
                     first:mt-0 last:mb-0 group transition-all hover:bg-darker-blue">
                         <h2 className="group-hover:text-white">Notification Settings</h2>
                         <svg xmlns="http://www.w3.org/2000/svg"
-                             className="h-5 w-5"
+                             className="h-5 w-5 group-hover:text-white"
                              viewBox="0 0 20 20" fill="currentColor">
                             <path d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z" />
                         </svg>
@@ -81,7 +81,9 @@ export default function Header(props: {
                     <li className="flex justify-between items-center p-2 rounded my-1
                     first:mt-0 last:mb-0 group transition-all hover:bg-darker-blue">
                         <h2 className="group-hover:text-white">Privacy Settings</h2>
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                        <svg xmlns="http://www.w3.org/2000/svg"
+                             className="h-5 w-5 group-hover:text-white"
+                             viewBox="0 0 20 20" fill="currentColor">
                             <path fillRule="evenodd" d="M2.166 4.999A11.954 11.954 0 0010 1.944 11.954 11.954 0 0017.834 5c.11.65.166 1.32.166 2.001 0 5.225-3.34 9.67-8 11.317C5.34 16.67 2 12.225 2 7c0-.682.057-1.35.166-2.001zm11.541 3.708a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
                         </svg>
                     </li>
@@ -89,14 +91,18 @@ export default function Header(props: {
                     <li className="flex justify-between items-center p-2 rounded my-1
                     first:mt-0 last:mb-0 group transition-all hover:bg-darker-blue">
                         <h2 className="group-hover:text-white">Edit Server Profile</h2>
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                        <svg xmlns="http://www.w3.org/2000/svg"
+                             className="h-5 w-5 group-hover:text-white"
+                             viewBox="0 0 20 20" fill="currentColor">
                             <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
                         </svg>
                     </li>
                     <li className="flex justify-between items-center p-2 rounded my-1
                     first:mt-0 last:mb-0 group transition-all hover:bg-darker-blue">
                         <h2 className="group-hover:text-white">Hide Muted Channels</h2>
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                        <svg xmlns="http://www.w3.org/2000/svg"
+                             className="h-6 w-6 group-hover:text-white"
+                             fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                             <path strokeLinecap="round" strokeLinejoin="round" d="M8 12h.01M12 12h.01M16 12h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                         </svg>
                     </li>
