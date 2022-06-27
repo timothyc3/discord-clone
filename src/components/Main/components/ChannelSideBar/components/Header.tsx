ebaf296580e62476216dc148fb62f92d771773c8
Tim <timothyc2@Gmail.com>
2022-06-27 21:06:37 +0800

    when header on channel sidebar toggles active the caret changes into cross and vice versa

--- src/components/Main/components/ChannelSideBar/components/Header.tsx
+++ src/components/Main/components/ChannelSideBar/components/Header.tsx
@@ -26,11 +26,18 @@ export default function Header(props: {
             <h2 className="font-semibold text-white font-body">
                 {heading}
             </h2>
-            <svg xmlns="http://www.w3.org/2000/svg"
-                 className="h-4 w-4 ml-auto text-light-grey"
-                 fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={3.5}>
+            {props.headerActive ?
+                <svg xmlns="http://www.w3.org/2000/svg"
+                     className="h-4 w-4 ml-auto text-light-grey"
+                     fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={3.5}>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+                :
+                <svg xmlns="http://www.w3.org/2000/svg"
+                  className="h-4 w-4 ml-auto text-light-grey"
+                  fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={3.5}>
                 <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7"/>
-            </svg>
+            </svg>}
         </div>
 
     )
