6a846fcfe4d23098f4ff34b1b4773c09ca192c8b
Tim <timothyc2@Gmail.com>
2022-06-29 21:39:49 +0800

    added lock svg to CreateChannelWindow.tsx

--- src/components/Main/components/CreateChannelWindow/CreateChannelWindow.tsx
+++ src/components/Main/components/CreateChannelWindow/CreateChannelWindow.tsx
@@ -55,6 +55,13 @@ export default function CreateChannelWindow() {
                             onChange={handleChannelNameTextInput}
                         />
                     </div>
+                    <div className="grid grid-rows-[2fr_3fr]">
+                        <svg xmlns="http://www.w3.org/2000/svg"
+                             className="h-5 w-5 text-light-grey"
+                             viewBox="0 0 20 20" fill="currentColor">
+                            <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
+                        </svg>
+                    </div>
                     <div className="bg-channel-hover-grey w-full h-[65px] px-4
                         flex items-center justify-between"
                     >
@@ -63,7 +70,6 @@ export default function CreateChannelWindow() {
                             Create
                         </button>
                     </div>
-
                 </div>
 
             </div>
