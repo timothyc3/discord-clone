af21d84745ab11c1b4869571aee777199e0c54a3
Tim <timothyc2@Gmail.com>
2022-06-29 21:10:38 +0800

    Added title and cross on top right for CreateChannelWindow.tsx

--- src/components/Main/components/CreateChannelWindow/CreateChannelWindow.tsx
+++ src/components/Main/components/CreateChannelWindow/CreateChannelWindow.tsx
@@ -20,14 +20,23 @@ export default function CreateChannelWindow() {
 
     return (
         <>
-            { active && <div className="bg-black/70 w-full h-full fixed flex justify-center items-center"
+            { active && <div className="bg-black/70 w-full h-full fixed flex justify-center items-center "
                              onClick={onExit}>
                 <div
-                    className="relative bg-white w-108 h-auto rounded-md"
+                    className="relative bg-channel-active-grey w-108 h-auto
+                    rounded-xl grid grid-rows-[auto_auto_200px_65px] p-4"
                     onClick={(event) => event.stopPropagation()}
                 >
+                    <h1 className="text-white font-semibold text-xl">Create Channel</h1>
+                    <svg xmlns="http://www.w3.org/2000/svg"
+                         className="h-7 w-7 absolute right-3.5 top-3.5 text-inactive-light-grey hover:text-server-bar-black
+                             transition-colors"
+                         onClick={onExit}
+                         fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12"/>
+                    </svg>
                     <input
-                        className="w-[90%] h-8 mt-2 pl-2 text-sm text-server-bar-black rounded bg-light-grey/40 outline-0
+                        className="w-full h-10 mt-2 pl-2 text-sm text-server-bar-black rounded bg-server-bar-black outline-0
                     self-center"
                         type="text" id="serverName" name="serverName" autoComplete="off"
                         placeholder={"new-channel"}
