fccd4199875c49b6681fd5b344b97c9b33cd4ca6
Tim <timothyc2@Gmail.com>
2022-07-26 22:04:39 +0800

    Added leave server button and cancel button to LeaveServerWindow.tsx

--- src/components/Main/components/LeaveServerWindow/LeaveServerWindow.tsx
+++ src/components/Main/components/LeaveServerWindow/LeaveServerWindow.tsx
@@ -25,18 +25,28 @@ export default function LeaveServerWindow() {
         <>
             {active && <div className="bg-black/70 w-full h-full fixed flex justify-center items-center"
                             onClick={onExit}>
-                <div className="relative w-108 h-56 rounded-md grid grid-rows-[6.5fr_3.5fr] overflow-hidden"
+                <div className="relative w-108 h-52 rounded-md grid grid-rows-[6.5fr_3.5fr] overflow-hidden"
                      onClick={(event) => {
                          event.stopPropagation();
                      }}>
                     <div className="bg-sub-black pt-4 px-4 text-white">
-                        <h2 className="font-bold text-lg">{`Leave ${serverName}`}</h2>
+                        <h2 className="font-bold text-lg">{`Leave '${serverName}'`}</h2>
                         <p className="text-sm pt-4">
                             Are you sure you want to leave <span className="font-semibold">{serverName}</span>?
                             You won't be able to re-join this server unless you are re-invited
                         </p>
                     </div>
-                    <div className="bg-server-bar-black"></div>
+                    <div className="bg-server-bar-black flex items-center justify-end gap-8 px-4">
+                        <button className="text-sm text-white font-semibold">
+                            Cancel
+                        </button>
+                        <button
+                            className='hover:bg-red-800 bg-red-500 h-10 w-28 rounded flex justify-center
+                            items-center text-white text-sm transition-all font-semibold'
+                        >
+                            Leave Server
+                        </button>
+                    </div>
                 </div>
             </div>}
         </>
